1954294e1f2c684e31a6f8cf363e5887d10ef265
Emrah Yurtlu <emrahyurtlu@gmail.com>
2020-03-08 23:03:46 +0300

    Home page eklendi.

--- src/main/java/com/company/controller/HomeController.java
+++ src/main/java/com/company/controller/HomeController.java
@@ -1,13 +1,13 @@
 package com.company.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @RequestMapping("/")
     public String home() {
-        return "Here is home page!";
+        return "views/index";
     }
 }
